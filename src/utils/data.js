479aca3bbbe3ec37d77dbac6b8edfa4c0b65bf01
FrancoPisapia <franco.pisapia405@gmail.com>
2022-11-23 23:16:15 -0300

    iniciada la data

---
+++ src/utils/data.js
@@ -0,0 +1,13 @@
+export const data =[
+    {
+
+        id:1,
+        title:'Armazon Vintage Slim Acetate TV727 50 20-145 7V',
+        src: '../Fotos de anteojos/lentes1.jpg',
+        genero: 'unisex',
+        coolorDelArmazon: 'Azul',
+        estilo: 'Redondo',
+        material: 'Acetato y metal'
+
+    }
+]
