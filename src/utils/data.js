bcb6574f3abff809e90c463c7334caabbaada6b6
FrancoPisapia <franco.pisapia405@gmail.com>
2022-11-24 20:16:09 -0300

    data terminado

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,13 +1,132 @@
 export const data =[
     {
-
         id:1,
         title:'Armazon Vintage Slim Acetate TV727 50 20-145 7V',
+        marca: "Vintage",
+        tipo:'lente',
         src: '../Fotos de anteojos/lentes1.jpg',
         genero: 'unisex',
         coolorDelArmazon: 'Azul',
         estilo: 'Redondo',
-        material: 'Acetato y metal'
+        material: 'Acetato y metal',
+        precio:1200
 
-    }
+    },
+    {
+        id:2,
+        title:'Armazon Vintage Slim Acetate TV727 50 20-149 7V',
+        marca: "Vintage",
+        tipo:'lente',
+        src: '../Fotos de anteojos/lentes2.jpg',
+        genero: 'unisex',
+        coolorDelArmazon: 'Transparente',
+        estilo: 'Redondo',
+        material: 'Acetato y metal',
+        precio:15000
+
+    },
+    {
+        id:3,
+        title:'Armazon Vintage Slim Acetate TV727 50 20-175 7V',
+        marca: "Vintage",
+        tipo:'lente',
+        src: '../Fotos de anteojos/lentes3.jpg',
+        genero: 'unisex',
+        coolorDelArmazon: 'Azul y verde petroleo',
+        estilo: 'Rectangular',
+        material: 'Acetato y metal',
+        precio: 14000
+
+    },
+    {
+        id:4,
+        title:'Armazón Moretti MORETTI "CLIP ON" B-Ultem hw908 ',
+        marca: "Moretti",
+        tipo:'sol',
+        src: '../Fotos de anteojos/lentes4.jpg',
+        genero: 'unisex',
+        coolorDelArmazon: 'Negro',
+        estilo: 'Rectangular',
+        material: 'Acetato y metal',
+        precio:22000
+
+    },
+    {
+        id:5,
+        title:'Lentes de sol  NEW YORK ',
+        marca: "Inifinit",
+        tipo:'sol',
+        src: '../Fotos de anteojos/lentes5.jpg',
+        genero: 'unisex',
+        coolorDelArmazon: 'Negro mate',
+        estilo: 'Rectangular',
+        material: 'Acetato',
+        precio:9000
+
+    },
+    {
+        id:6,
+        title:'Lentes de sol  TULUM ',
+        marca: "Inifinit",
+        tipo:'sol',
+        src: '../Fotos de anteojos/lentes6.jpg',
+        genero: 'unisex',
+        coolorDelArmazon: 'Negro mate',
+        estilo: 'Redondos',
+        material: 'Acetato',
+        precio:7500
+
+    },
+    {
+        id:7,
+        title:'Lentes de sol Z-Ultem bz407 ',
+        marca: "Moretti",
+        tipo:'sol',
+        src: '../Fotos de anteojos/lentes7.jpg',
+        genero: 'unisex',
+        coolorDelArmazon: 'Negro ',
+        estilo: 'Rectangular',
+        material: 'Acetato y metal',
+        precio:15000
+
+    },
+    {
+        id:8,
+        title:'Lentes de sol C-Ultem pl131 ',
+        marca: "Moretti",
+        tipo:'sol',
+        src: '../Fotos de anteojos/lentes7.jpg',
+        genero: 'unisex',
+        coolorDelArmazon: 'Dorado ',
+        estilo: 'Aviador',
+        material: 'Metal',
+        precio:22000
+
+    },
+    {
+        id:9,
+        title:'Lentes de contacto multifocal ',
+        marca: "Optix",
+        tipo:'Lente de contacto',
+        src: '../Fotos de anteojos/lentes9.jpg',
+        genero: 'unisex',
+        coolorDelArmazon: '',
+        estilo: '',
+        material: 'Silicona',
+        precio:8000
+
+    },
+    {
+        id:10,
+        title:'Lentes de contacto astigmatismo ',
+        marca: "Optix",
+        tipo:'Lente de contacto',
+        src: '../Fotos de anteojos/lentes10.jpg',
+        genero: 'unisex',
+        coolorDelArmazon: '',
+        estilo: '',
+        material: 'Silicona',
+        precio:5000
+
+    },
 ]
