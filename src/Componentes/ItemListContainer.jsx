993501e35c219f0440f224d6e98e03ef5a1e1f1e
FrancoPisapia <franco.pisapia405@gmail.com>
2022-11-25 18:28:49 -0300

    terminado

--- src/Componentes/ItemListContainer.jsx
+++ src/Componentes/ItemListContainer.jsx
@@ -1,15 +1,15 @@
 
 const ItemListContainer = (prop) =>{
     return(
-
+        <>
         <div className="container">{prop.greeting}</div>
-        /*<ul className="listaProductos">
+        {/*<ul className="listaProductos">
         <li className="">{prop.producto1}</li>
         <li>{prop.producto2}</li>
         <li>{prop.producto3}</li>
         <li>{prop.producto4}</li>
-        </ul>*/
-
+        </ul>*/}
+        </>
     )
 }
 
